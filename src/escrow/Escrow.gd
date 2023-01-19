e305368440ceea5e717557aff82731cc7bbfbc5f
Fábio Oliveira <roxy2298gm@gmail.com>
2023-01-19 16:27:38 -0300

    add transitions / add transition CircleInPlayer / add transition HorizontalCut (incompleto)

--- src/escrow/Escrow.gd
+++ src/escrow/Escrow.gd
@@ -1,2 +1,12 @@
 extends Node2D
+ 
 
+func lerp(A, B, t):
+	return A * (1 - t) + B * t
+	
+func _ready():
+	var player = Vector2(0.5, 0.5)
+	var ratio = 1024/600
+	var test = lerp(0.5, 0.0, ratio)
+	print(Vector2(test, 0).distance_to(player))
+	print(test)
