29f1906e40a851575d0a4f7051ee0cdfd670e75a
Fábio Oliveira <roxy2298gm@gmail.com>
2023-01-20 10:38:36 -0300

    add transition vertical_cut / add transition diamond_pixel

--- src/escrow/Escrow.gd
+++ src/escrow/Escrow.gd
@@ -5,8 +5,5 @@ func lerp(A, B, t):
 	return A * (1 - t) + B * t
 	
 func _ready():
-	var player = Vector2(0.5, 0.5)
-	var ratio = 1024/600
-	var test = lerp(0.5, 0.0, ratio)
-	print(Vector2(test, 0).distance_to(player))
-	print(test)
+	var x = 0.0
+	print(x - floor(x))
